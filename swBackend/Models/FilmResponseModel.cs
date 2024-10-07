95c569990a1ea813d041b3a627d50abfd3076722
cecifernandez <mceciliafernandezst@gmail.com>
2024-10-06 23:41:27 -0300

    first com

---
+++ swBackend/Models/FilmResponseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace swBackend.Models
+{
+    public class FilmResponseModel
+    {
+            public int Count { get; set; }
+            public string Next { get; set; }
+            public string Previous { get; set; }
+            public IEnumerable<FilmModel> Results { get; set; } 
+
+    }
+}
